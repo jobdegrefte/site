3e522da3c4d1dcc4ea791d6808f54cf71e6d5e5e
jobdegrefte <jobdegrefte@gmail.com>
2026-04-20 13:23:34 +0200

    Add files via upload

--- global.js
+++ global.js
@@ -4,23 +4,43 @@ $(function() {
 });
 
 function mobileNav() {
-  $('.nav-toggle').on('click', function(){
-    var status = $(this).hasClass('is-open');
-    if(status){ $('.nav-toggle, .mobilenav, .header').removeClass('is-open'); }
-    else { $('.nav-toggle, .mobilenav, .header').addClass('is-open'); }
+  function toggleMenu() {
+    var isOpen = $('.nav-toggle').hasClass('is-open');
+    if (isOpen) {
+      $('.nav-toggle, .mobilenav').removeClass('is-open');
+      $('.nav-toggle').attr('aria-expanded', 'false');
+      $('.nav-toggle').attr('aria-label', 'Open navigation');
+    } else {
+      $('.nav-toggle, .mobilenav').addClass('is-open');
+      $('.nav-toggle').attr('aria-expanded', 'true');
+      $('.nav-toggle').attr('aria-label', 'Close navigation');
+    }
+  }
+
+  $('.nav-toggle').on('click', toggleMenu);
+
+  $('.nav-toggle').on('keydown', function(event) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      toggleMenu();
+    }
+  });
+
+  $('.mobilenav a').on('click', function() {
+    $('.nav-toggle, .mobilenav').removeClass('is-open');
+    $('.nav-toggle').attr('aria-expanded', 'false');
+    $('.nav-toggle').attr('aria-label', 'Open navigation');
   });
 }
 
-function smoothScroll (duration) {
-	$('a[href^="#"]').on('click', function(event) {
-
-	    var target = $( $(this).attr('href') );
-
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
-	});
+function smoothScroll(duration) {
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $($(this).attr('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
 }
